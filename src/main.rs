e8e6c7c60ecd75eaf207834e11f6c0ff218609df
Heiko Schaefer <heiko@schaefer.name>
2022-12-25 16:58:13 +0100

    Generate revocation certificates

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,21 @@
 
 use std::collections::HashMap;
 use std::io::Write;
+use std::time::SystemTime;
 
 use anyhow::Result;
+use chrono::{DateTime, Utc};
 use clap::{Parser, ValueEnum};
 use openpgp_card::algorithm::AlgoSimple;
 use openpgp_card::{card_do::TouchPolicy, Error, KeyType, StatusBytes};
 use openpgp_card_pcsc::PcscBackend;
-use openpgp_card_sequoia::{state::Open, state::Transaction, util, Card};
+use openpgp_card_sequoia::{state::Open, state::Transaction, util, Card, PublicKey};
 use rand::Rng;
+use sequoia_openpgp::cert::CertRevocationBuilder;
+use sequoia_openpgp::packet::{Signature, UserID};
 use sequoia_openpgp::serialize::Serialize;
+use sequoia_openpgp::types::ReasonForRevocation;
+use sequoia_openpgp::{armor, Cert, Packet};
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -55,6 +61,12 @@ pub struct Cli {
     /// Filename of the output zip archive.
     #[clap(long = "output")]
     pub output: String,
+
+    /// Card ident.
+    ///
+    /// Optional, if unset any blank card that is found will be initialized.
+    #[clap(long = "card")]
+    pub card: Option<String>,
 }
 
 #[derive(ValueEnum, Debug, Clone)]
@@ -119,10 +131,89 @@ fn card_empty(open: &Card<Transaction>) -> Result<()> {
     }
 }
 
+// Generate one hard and one soft revocation (with the current timestamp)
+fn revocations(
+    cert: &Cert,
+    key_sig: PublicKey,
+    open: &mut Card<Transaction>,
+) -> Result<(Vec<u8>, Vec<u8>)> {
+    fn revoc_to_armored(
+        sig: &Signature,
+        headers: Option<Vec<(String, String)>>,
+    ) -> Result<Vec<u8>> {
+        let mut buf = vec![];
+
+        let rev = Packet::Signature(sig.clone());
+
+        let mut writer = armor::Writer::with_headers(
+            &mut buf,
+            armor::Kind::PublicKey,
+            headers.unwrap_or_default(),
+        )?;
+        rev.export(&mut writer)?;
+        writer.finalize()?;
+
+        Ok(buf)
+    }
+
+    // helper: use the card to perform a signing operation
+    let mut sign_on_card =
+        |op: &mut dyn Fn(&mut dyn sequoia_openpgp::crypto::Signer) -> Result<Signature>| {
+            // Allow signing on the card
+            open.verify_user_for_signing(PW1)?;
+            if let Some(mut sign) = open.signing_card() {
+                // Card-backed signer for bindings
+                let mut card_signer = sign.signer_from_public(key_sig.clone(), &|| {});
+
+                // Make signature, return it
+                let s = op(&mut card_signer)?;
+                Ok(s)
+            } else {
+                Err(anyhow::anyhow!("Failed to open card for signing"))
+            }
+        };
+
+    let now = SystemTime::now();
+
+    let dt: DateTime<Utc> = now.into();
+    let date = dt.format("%Y-%m-%d");
+
+    // hard revocation
+    let s = sign_on_card(&mut |signer| {
+        CertRevocationBuilder::new()
+            .set_signature_creation_time(now)?
+            .set_reason_for_revocation(
+                ReasonForRevocation::KeyCompromised,
+                b"Certificate has been compromised",
+            )?
+            .build(signer, cert, None)
+    })?;
+    let header = vec![(
+        "Comment".to_string(),
+        format!("Hard revocation (certificate compromised) ({})", date),
+    )];
+    let hard = revoc_to_armored(&s, Some(header))?;
+
+    // soft revocation
+    let s = sign_on_card(&mut |signer| {
+        CertRevocationBuilder::new()
+            .set_signature_creation_time(now)?
+            .set_reason_for_revocation(ReasonForRevocation::KeyRetired, b"Certificate retired")?
+            .build(signer, cert, None)
+    })?;
+    let header = vec![(
+        "Comment".to_string(),
+        format!("Soft revocation (certificate retired) ({})", date),
+    )];
+    let soft = revoc_to_armored(&s, Some(header))?;
+
+    Ok((hard, soft))
+}
+
 fn init(
     open: &mut Card<Transaction>,
     name: &str,
-    _email: &str,
+    email: &str,
     touch_policy: TouchPolicy,
 ) -> Result<HashMap<String, Vec<u8>>> {
     // We know that there is no key material on the card
@@ -160,23 +251,28 @@ fn init(
     let aut = open.public_key(KeyType::Authentication)?;
 
     // Generate a public key "Cert" representation of the key material
-    // FIXME: pass User IDs (split and combined) as parameters
-    // FIXME: set expiration?
+
+    // make a traditional "combined" User ID from name/email parameters
+    let uid = UserID::from_address(name.into(), None, email)?;
+
+    // FIXME: set expiration? (default: 2y, optional param?)
+
     let cert = util::make_cert(
         open,
-        sig.expect("Signature key missing on card"),
+        sig.clone().expect("Signature key missing on card"),
         dec,
         aut,
         Some(PW1),
         &|| {},
         &|| {},
-        &[],
+        &[uid.to_string()],
     )?;
 
     let mut pubkey: Vec<u8> = vec![];
     cert.armored().serialize(&mut pubkey)?;
 
-    // FIXME: generate (and export) revocation certificate(s) for this Cert?
+    // Generate one hard and one soft revocation certificate for this Cert
+    let (hard, soft) = revocations(&cert, sig.expect("Signature key missing on card"), open)?;
 
     println!("- Temporarily disabling touch confirmation for attestation ...");
 
@@ -228,6 +324,8 @@ fn init(
     // Collect data to save as files: pubkey, attestations, attestation cert
     let files = HashMap::from([
         (format!("{}.pub", file_ident), pubkey),
+        (format!("{}.revocation.soft", file_ident), soft),
+        (format!("{}.revocation.hard", file_ident), hard),
         (
             format!("{}-sig.attestation", file_ident),
             pem_encode(att_sig).as_bytes().to_vec(),
@@ -246,7 +344,11 @@ fn init(
         ),
     ]);
 
-    println!("- Configuring touch confirmation for all key slots ...");
+    println!(
+        "- Configuring touch confirmation for key slots to '{}' ...",
+        touch_policy
+    );
+
     {
         // Set touch confirmation for all key slots to "Fixed"
         // (to allow non-interactive attestation generation)
@@ -272,6 +374,8 @@ fn init(
             } else {
                 return Err(e.into());
             }
+        } else {
+            println!("  Touch confirmation for attestation key slot set to 'On'",);
         }
     }
 
